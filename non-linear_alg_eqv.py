0a9a7984cbf5e9cd274c7e32149f13ffd6c27e30
Nina Bondarchuk <nibo@simcorp.com>
2021-05-16 19:43:57 +0300

    Add project files.

---
+++ non-linear_alg_eqv.py
@@ -0,0 +1,70 @@
+class Alg_eqv:
+    def __init__(self, function):
+        self.function = function
+
+    def set_derivative(self, derivative):
+        self.derivative = derivative
+
+    def bisection(self, left, right, eps):
+        print("\n\nCalling bisection method for interval [" + str(left) + ", " + str(right) +"].\n")
+        f = self.function
+        iter = 1
+        while right - left > eps or abs(f((left + right)/2)) > eps:
+            mid = (right + left)/2
+            if f(mid) == 0:
+                break
+            if f(left)*f(mid) < 0:
+                right = mid
+            else:
+                left = mid
+            print("Iteration " + str(iter) +".\t Sector: [" + "%.6f" % left + ", " + "%.6f" % right + "],\t \t f(" + "%.6f" % left +") = "  + "%.6f" % f(left) +", \t f(" + "%.6f" % right + ") = " + "%.6f" % f(right))
+            iter += 1
+        return (right + left)/2
+
+    def newton(self, x_0, eps):
+        print("\n\nCalling Newton method with first approximation " + str(x_0) + ".\n")
+        f, df = self.function, self.derivative
+        approximations = [x_0]
+        iter = 1
+        while True:
+            x_k = approximations[-1]
+            approximations.append(x_k - f(x_k)/df(x_k))
+            if abs(x_k - approximations[-1]) < eps and abs(f(approximations[-1])) < eps:
+                break
+            print("Iteration " + str(iter) +".\t Approximation:  = "  + "%.6f" % approximations[-1] +", \t\t f(" + "%.6f" % approximations[-1] +  ") = %.6f" % f(approximations[-1]))
+            iter += 1
+        return approximations[-1]
+
+    def chord(self, left, right, eps):
+        print("\n\nnCalling chord method for interval [" + str(left) + ", " + str(right) +"].\n")
+        f = self.function
+        approximations = [left]
+        iter = 1
+        while True:
+            x_k = (left * f(right) - right* f(left))/(f(right) - f(left))
+            if abs(approximations[-1] - x_k) < eps and abs(f(x_k)) < eps:
+                break
+            if f(left)*f(x_k) < 0:
+                right = x_k
+            else:
+                left = x_k
+            print("Iteration " + str(iter) +".\t Sector: [" + "%.6f" % left + ", " + "%.6f" % right + "],\t \t f(" + "%.6f" % left +") = "  + "%.6f" % f(left) +", \t f(" + "%.6f" % right + ") = " + "%.6f" % f(right))
+            approximations.append(x_k)
+            iter += 1
+        return x_k
+
+def polynom(x):
+    return x**4 - 4*x**3 + 4*x**2 + 3 * x - 3
+
+def dpoly(x):
+    return 4*x**3 - 12*x**2 + 8*x + 3
+
+f = Alg_eqv(polynom)
+f.set_derivative(dpoly)
+
+print("Bisection method result:", "%.6f" % f.bisection(-1, 0, 0.00001))
+print("Bisection method result:", "%.6f" % f.bisection(0, 1, 0.00001))
+print("Chord method result:", "%.6f" % f.chord(-1, 0, 0.00001))
+print("Chord method result:", "%.6f" % f.chord(0, 1, 0.00001))
+print("Newton method result:", "%.6f" % f.newton(-1, 0.00001))
+print("Newton method result:", "%.6f" % f.newton(1, 0.00001))
